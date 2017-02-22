63ecccc45855502270a493ecd673ad33953cba03
Julius Koronci <koronci@trimbroker.com>
2017-02-22 23:02:49 +0100

    Refactoring data validation

--- Fetch.js
+++ Fetch.js
@@ -5,12 +5,6 @@ function fakeResponse(data: {
     'body': Object,
     'status_code': ?number
 }): Response {
-    if (typeof data.body === 'undefined') {
-        throw new Error('Mock Data have no body!');
-    }
-    if (data.status_code && data.status_code >= 500) {
-        throw new Error('Mocking Server Error');
-    }
     return new Response(JSON.stringify(data.body), {
         'status': data.status_code || 200,
         headers: {
@@ -20,7 +14,18 @@ function fakeResponse(data: {
     })
 }
 
-const mockFetch = _.compose((f) => new Promise(f), _.curry((response, resolve) => resolve(response)), fakeResponse);
+function validateData(data) {
+    if (typeof data.body === 'undefined') {
+        throw new Error('Mock Data have no body!');
+    }
+    if (data.status_code && data.status_code >= 500) {
+        throw new Error('Mocking Server Error');
+    }
+
+    return data;
+}
+
+const mockFetch = _.compose((f) => new Promise(f), _.curry((response, resolve) => resolve(response)), fakeResponse, validateData);
 
 
 const fetchReplacement = _.curry((oldFetch: Function, mockData: Object, input: string, config: Object) => {
--- index.js
+++ index.js
@@ -11,12 +11,6 @@ var _ramda2 = _interopRequireDefault(_ramda);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function fakeResponse(data) {
-    if (!data.body) {
-        throw new Error('Mock Data have no body!');
-    }
-    if (data.status_code && data.status_code >= 500) {
-        throw new Error('Mocking Server Error');
-    }
     return new Response(JSON.stringify(data.body), {
         'status': data.status_code || 200,
         headers: {
@@ -27,11 +21,22 @@ function fakeResponse(data) {
 }
 
 
+function validateData(data) {
+    if (typeof data.body === 'undefined') {
+        throw new Error('Mock Data have no body!');
+    }
+    if (data.status_code && data.status_code >= 500) {
+        throw new Error('Mocking Server Error');
+    }
+
+    return data;
+}
+
 var mockFetch = _ramda2.default.compose(function (f) {
     return new Promise(f);
 }, _ramda2.default.curry(function (response, resolve) {
     return resolve(response);
-}), fakeResponse);
+}), fakeResponse, validateData);
 
 var fetchReplacement = _ramda2.default.curry(function (oldFetch, mockData, input, config) {
     return mockData[input] ? mockFetch(mockData[input]) : oldFetch(input, config);
